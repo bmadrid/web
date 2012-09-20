b7d5e48aa8825a0510319ad46dda6128083787d1
Erik Madrid <emadrid@aptus.io>
2012-09-20 01:39:35 -0500

    emailer is working when called from index.php

--- emailer.php
+++ emailer.php
@@ -11,42 +11,63 @@
     $_POST[$k]=htmlspecialchars(strip_tags($_POST[$k]));
   }
 
-  $name = empty( $_POST[ "name" ] ) ? "*Not Specified*" : $_POST[ "name" ];
-  $email = empty( $_POST[ "email" ] ) ? "*Not Specified*" : $_POST[ "email" ];
-  $age = empty( $_POST[ "age" ] ) ? "*Not Specified*" : $_POST[ "age" ];
-  $diagnosis = empty( $_POST[ "diagnosis" ] ) ? "*Not Specified*" : $_POST[ "diagnosis" ];
-  $needs = empty( $_POST[ "needs" ] ) ? "*Not Specified*" : $_POST[ "needs" ];
-  $interest = empty( $_POST[ "interest" ] ) ? "*Not Specified*" : $_POST[ "interest" ];
-  $referer = empty( $_POST[ "referer" ] ) ? "*Not Specified*" : $_POST[ "referer" ];
+  $invalid_request = empty( $_POST[ "name"] ) || empty( $_POST[ "email" ] );
 
-  $message = "<p>Name: " . $name . "</p>" .
-    "<p>Email: " . $email . "</p>" .
-    "<p>Age: " . $age . "</p>" .
-    "<p>Diagnosis: " . $diagnosis . "</p>" .
-    "<p>Needs: " . $needs . "</p>" .
-    "<p>Interest: " . $interest . "</p>" .
-    "<p>Referer: " . $referer . "</p>";
+  if ( $invalid_request ) {
 
-  $mail = new PHPMailer(); //true to output debug
-  $mail->IsSMTP();
+    echo "You're not supposed to be here...";
 
-  try {
-    $mail->Host       = "ssl://smtp.gmail.com";
-    $mail->SMTPDebug  = 1;
-    $mail->SMTPAuth   = true;
-    $mail->Host       = "ssl://smtp.gmail.com";
-    $mail->Port       = 465;
-    $mail->Username   = "bmadrid@spectrumprek.com";
-    $mail->Password   = "TEAMSPECTRUM2012";
-    $mail->AddAddress('contact@spectrumprek.com', 'Bethany Madrid');
-    $mail->SetFrom('contact@spectrumprek.com', 'Survey');
-    $mail->Subject = 'PHPMailer Test Subject via mail(), advanced';
-    $mail->MsgHTML($message);
-    $mail->Send();
+  }
+  else {
 
-  } catch (phpmailerException $e) {
-    echo $e->errorMessage();
-  } catch (Exception $e) {
-    echo $e->getMessage();
+    $name = $_POST[ "name" ];
+    $email = $_POST[ "email" ];
+    $age = empty( $_POST[ "age" ] ) ? "*Not Specified*" : $_POST[ "age" ];
+    $diagnosis = empty( $_POST[ "diagnosis" ] ) ? "*Not Specified*" : $_POST[ "diagnosis" ];
+    $needs = empty( $_POST[ "needs" ] ) ? "*Not Specified*" : $_POST[ "needs" ];
+    $intervention = $_POST[ "intervention" ];
+    $aba =  $_POST[ "aba" ];
+    $referer = empty( $_POST[ "referer" ] ) ? "*Not Specified*" : $_POST[ "referer" ];
+
+    $message = "<p>Name: " . $name . "</p>" .
+      "<p>Email: " . $email . "</p>" .
+      "<p>Age: " . $age . "</p>" .
+      "<p>Diagnosis: " . $diagnosis . "</p>" .
+      "<p>Needs: " . $needs . "</p>" .
+      "<p>Intervention: " . $intervention . "</p>" .
+      "<p>ABA: " . $aba . "</p>" .
+      "<p>Referer: " . $referer . "</p>";
+
+    $mail = new PHPMailer(); //true to output debug
+    $mail->IsSMTP();
+
+    $accounts_raw = file_get_contents("accounts.json");
+    $email_settings = json_decode($accounts_raw, true);
+
+    echo $email_settings[ "username" ];
+
+    try {
+      $mail->Host       = "ssl://smtp.gmail.com";
+      $mail->SMTPDebug  = 1;
+      $mail->SMTPAuth   = true;
+      $mail->Host       = "ssl://smtp.gmail.com";
+      $mail->Port       = 465;
+      $mail->Username   = $email_settings[ "username" ];
+      $mail->Password   = $email_settings[ "password" ];
+      // $mail->AddAddress( $email_settings[ "to" ], $email_settings[ "name" ] );
+      // $mail->SetFrom( $email_settings[ "from" ] , $email_settings[ "name" ] );
+
+      $mail->AddAddress( $email_settings[ "to" ], $email_settings[ "name" ] );
+      $mail->SetFrom( $email_settings[ "from" ] , $email_settings[ "name" ] );
+      
+      $mail->Subject = '[Survey] from ' . $name;
+      $mail->MsgHTML($message);
+      $mail->Send();
+
+    } catch (phpmailerException $e) {
+      echo $e->errorMessage();
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
 ?>
--- index.php
+++ index.php
@@ -2,19 +2,102 @@
 
   <head>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" type="text/css" />
     <script>
       $(document).ready(function() {
-        
-        $( '#name' ).val( 'Erik' );
-        $( '#email' ).val( 'bmadrid@spectrumprek.com' );
-        $( '#age' ).val( '3' );
-        $( '#diagnosis' ).val( 'autism' );
-        $( '#needs' ).val( 'these are some things that we need' );
-        $( '#interest' ).val( 'yes' );
-        $( '#referer' ).val( 'internet' );
 
-      });
+        $( '#submit' ).button()
+        
+        $( '#submit' ).click( function() {
+
+          $( '#submit' ).button( "option", "disabled", true );
+          $( '#email' ).attr( "disabled", "disabled" );
+          $( '#age' ).attr( "disabled", "disabled" );
+          $( '#diagnosis' ).attr( "disabled", "disabled" );
+          $( '#needs' ).attr( "disabled", "disabled" );
+          $( '#interventionYes' ).attr( "disabled", "disabled" );
+          $( '#interventionNo' ).attr( "disabled", "disabled" );
+          $( '#abaYes' ).attr( "disabled", "disabled" );
+          $( '#abaNo' ).attr( "disabled", "disabled" );
+          $( '#referer' ).attr( "disabled", "disabled" );
+          
+          if ( validate() ) {
+
+            var intervention = $("input[name='intervention']:checked").val();
+            intervention = intervention == undefined || intervention == 'no' ? 'no' : 'yes';
+
+            var aba =  $("input[name='aba']:checked").val();
+            aba = aba == undefined || aba == 'no' ? 'no' : 'yes';
+
+            alert(intervention);
+
+            return false;
+
+            $.ajax({
+              type: "POST",
+              url: "emailer.php",
+              data: { 
+                name: $( '#name' ).val(), 
+                email: $( '#email' ).val(),
+                age: $( '#age' ).val(),
+                diagnosis: $( '#diagnosis' ).val(),
+                needs: $( '#needs' ).val(),
+                intervention: intervention,
+                aba: aba,
+                referer: $( '#referer' ).val()
+              },
+              beforeSend: function() {
+                $( '#submit' ).attr( 'disabled', 'disabled' );
+              },
+              success: function( data, textStatus, xhr ) {
+                alert( 'Thanks!' );
+              },
+              error: function( jqXHR, exception ) {
+                alert( 'oops, there was an error' + jqXHR.responseText );
+              }
+            });
+          }
+          else {
+            return false;
+          }
+        }); //submit
+
+      }); //document ready
+
+      function validate() {
+
+        var valid_data = true;
+        var error = '';
+
+        if ( $('#name').val().length == 0 ) {
+          error += 'Please enter your name.';
+          valid_data = false;
+        }
+        if ( $( '#email').val().length == 0 ) {
+          error += 'Please enter your email.';
+          valid_data = false;
+        }
+        return valid_data;
+      }
+
     </script>
+
+    <style type="text/css">
+
+      body { font-family: "Arial"; font-size: 10pt; }
+      center { margin-bottom: 20px; }
+      .error { color: #FF4500; }
+      #name, #email, #age, #referer 
+        { width: 445px; }
+
+      label { margin-left: 380px; float: left; width: 500px; }
+      textarea { margin-left: 400px; }
+      input { margin-left: 400px; }
+
+      #submit { margin-left: 380px; }
+
+    </style>
   </head>
 
   <body>
@@ -33,48 +116,39 @@
       We're here for you.
     </center>
 
-    <form id="contact-form" name="contact-form" method="post" action="emailer.php">
-
-      <label for="name">Name:</label>
-      <input id="name" type="text" />
-      <br />
-      <br />
-
-      <label for="email">Email:</label>
-      <input id="email" type="text" />
-      <br />
-      <br />
-
-      <label for="age">How old is your child?</label>
-      <input id="age" />
-      <br />
-      <br />
-
-      <label for="diagnosis">What is your child's specific diagnosis?</label>
-      <input id="diagnosis" />
-      <br />
-      <br />
-
-      <label for="needs">What are your family's needs?</label>
-      <input id="needs" />
-      <br />
-      <br />
-
-      <label for="interest">Would you be interested in meeting us to explore the possibility of receiving in-home early intervention services and ABA therapy with our team?</label>
-      <input id="interest" />
-      <br />
-      <br />
-
-      <label for="referer">How did you hear about us?</label>
-      <input id="referer" />
-      <br />
-      <br />
-
-      <input type="submit" text="submit" />
-
-      <p id="error">error here</p>
-
-    </form>
+    <label id="nameLabel" for="name">*Name:</label><br />
+    <input id="name" name="name" type="text" /><br /><br />
+    
+    <label id="emailLabel" for="email">*Email:</label><br />
+    <input id="email" name="email" type="text" /><br /><br />
+    
+    <label id="ageLabel" for="age">How old is your child? (y/m)</label><br />
+    <input id="age" /><br /><br />
+    
+    <label id="diagnosisLabel" for="diagnosis">What is your child's specific diagnosis?</label><br />
+    <textarea id="diagnosis" rows="3" cols="62"></textarea><br /><br />
+  
+    <label id="needsLabel" for="needs">What are your family's needs?</label><br />
+    <textarea id="needs" rows="3" cols="62"></textarea><br /><br />
+  
+    <label id="interventionLabel" for="intervention">Are you interested in receiving in-home early intervention services?</label><br />
+    <input id="interventionYes" type="radio" name="intervention" value="yes" /> Yes
+    <div>
+      <input id="interventionNo" type="radio" name="intervention" value="no" /> No
+    </div><br />a
+    
+    <label id="abaLabel" for="aba">Are you interested in receiving ABA therapy?</label><br />
+    <input id="abaYes" type="radio" name="aba" value="yes" /> Yes
+    <div>
+      <input id="abaNo" type="radio" name="aba" value="no" /> No
+    </div><br />
+    
+    <label for="referer">How did you hear about us?</label><br />
+    <input id="referer" name="referer" type="text" /><br /><br />
+    
+    <input id="submit" type="button" value="submit" /><br />
+  
+    <div id="error">error here</div>
 
   </body>
 
